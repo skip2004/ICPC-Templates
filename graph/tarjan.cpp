27cef8a2cf075f0811b2aa04c903ba56401f6434
p_b_p_b <bo.peng@stu.pku.edu.cn>
2023-10-20 11:29:19 +0800

    fix: tarjan

--- graph/tarjan.cpp
+++ graph/tarjan.cpp
@@ -1,9 +1,9 @@
 int dfn[sz],low[sz],cc;
 stack<int>S; int in[sz];
 int bel[sz],T;
-void dfs(int x,int fa) {
+void dfs(int x) {
 	dfn[x]=low[x]=++cc; S.push(x),in[x]=1;
-	for (auto v:V[x]) if (v!=fa) {
+	for (auto v:V[x]) {
 		if (!dfn[v]) dfs(v,x),chkmin(low[x],low[v]);
 		else if (in[v]) chkmin(low[x],dfn[v]);
 	}
