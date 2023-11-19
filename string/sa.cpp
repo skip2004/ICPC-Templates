be4ada67ef309c171f951f424da1d8d842a42b6e
FSYo <3414513905@qq.com>
2023-11-20 00:06:32 +0800

    i don't understand sa

--- string/sa.cpp
+++ string/sa.cpp
@@ -3,20 +3,21 @@ void Sort(){
 	for(int i=1;i<=m;i++) c[i] = 0;
 	for(int i=1;i<=n;i++) c[rk[i]]++;
 	for(int i=1;i<=m;i++) c[i] += c[i-1];
-	for(int i=n;i>=1;i--) SA[c[rk[y[i]]]--] = y[i];
+	for(int i=n;i>=1;i--) sa[c[rk[y[i]]]--] = y[i];
 }
-void get_SA(){
-	for(int i=1;i<=n;i++) rk[i] = s[i], y[i] = i;
-	Sort();
+void get_sa(){
+	for(int i=1;i<=n;i++) rk[i] = s[i], y[i] = i; Sort();
 	for(int k=1;k<=n;k<<=1){
 		int ret = 0;
 		for(int i=n-k+1;i<=n;i++) y[++ret] = i;
-		for(int i=1;i<=n;i++) if(SA[i] > k) y[++ret] = SA[i] - k;
-		Sort(); swap(rk,tmp); rk[SA[1]] = 1; int num = 1;
+		for(int i=1;i<=n;i++) if(sa[i] > k) y[++ret] = sa[i] - k;
+		Sort(); 
+		for(int i = 1; i <= n; i++) swap(rk[i], tmp[i]); 
+		rk[sa[1]] = 1; int num = 1;
 		for(int i=2;i<=n;i++){
-			if(tmp[SA[i]] == tmp[SA[i-1]] && tmp[SA[i]+k] == tmp[SA[i-1]+k])
-				rk[SA[i]] = num;
-			else rk[SA[i]] = ++num;
+			if(tmp[sa[i]] == tmp[sa[i-1]] && tmp[sa[i]+k] == tmp[sa[i-1]+k])
+				rk[sa[i]] = num;
+			else rk[sa[i]] = ++num;
 		} m = num;
 	}
 }
@@ -24,7 +25,7 @@ void get_h(){
 	int k = 0;
 	for(int i=1;i<=n;i++){
 		if(rk[i]==1) continue;
-		int j = SA[rk[i]-1]; if(k) k--;
+		int j = sa[rk[i]-1]; if(k) k--;
 		while(i+k<=n && j+k<=n && s[i+k]==s[j+k]) k++;
 		h[rk[i]] = k;
 	}
