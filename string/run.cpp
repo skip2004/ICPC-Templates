54b8af039cc4086a41461346fa3c6e276f4a3af9
FSYo <3414513905@qq.com>
2023-12-06 15:46:41 +0800

    runs

--- string/run.cpp
+++ string/run.cpp
@@ -1,19 +1,22 @@
+// need lcp and lcs 
 bool cmp(int x, int y){
 	int l = lcp(x, y);
 	if(x + l > n) return true; 
 	if(y + l > n) return false; 
 	return S[x + l] < S[y + l];
 }
-map<pi, int> ex; 
-void ins(int l, int r){
-	int p = r - l;
-	int l1 = lcp(l, r);
-	int l2 = lcs(l - 1, r - 1);
-	int L = l - l2, R = r + l1 - 1;
-	if(ex[pi(L, R)]) return;
-	ex[pi(L, R)] = true;
-	if(R - L + 1 >= 2 * p) 
-		t[++k] = (run){L, R, p};
+set <pi> ex; 
+void ins(int l, int r) {
+    int p = r - l;
+    int l1 = lcp(l, r);
+    int l2 = lcs(l - 1, r - 1);
+    int L = l - l2, R = r + l1 - 1; 
+    if(R - L + 1 >= 2 * p) {
+        auto iter = ex.lower_bound(pi(L, R));
+        if(iter != ex.end() && *iter == pi(L, R)) return ;
+        ex.emplace_hint(iter, pi(L, R));
+        runs.pb((run){L, R, p}); 
+    }
 }
 void Run(int o){
 	static int s[N];
