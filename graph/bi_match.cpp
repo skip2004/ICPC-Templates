c35279a6ba23eed59d170a6e1d355cb0922145e8
skip2004 <1228412312@qq.com>
2023-12-07 14:41:58 +0800

    update HK & barrett

--- graph/bi_match.cpp
+++ graph/bi_match.cpp
@@ -1,4 +1,4 @@
-// 匈牙利，左到右单向边，O(M|match|)
+// 匈牙利，左到右单向边，$O(M|match|)$
 std::vector<int> edge[N];
 bool dfs(int x, std::vector<int> & vis, std::vector<int> & match) {
 	for(int y : edge[x]) if(!vis[y]) 
@@ -13,7 +13,7 @@ std::vector<int> match(int nl, int nr) {
 	for(int i = 1;i <= nr;++i) ret[match[i]] = i;
 	return ret[0] = 0, ret;
 }
-/* 最小边覆盖，很可能没用，真别抄，抄这个不用抄 match */
+// 最小边覆盖
 std::pair<std::vector<int>, std::vector<int>> minedgecover(int nl, int nr) {
 	std::vector<int> vis(nr + 1), match(nr + 1), ret(nl + 1);
 	for(int i = 1;i <= nl;++i) if(dfs(i, vis, match)) 
@@ -26,9 +26,7 @@ std::pair<std::vector<int>, std::vector<int>> minedgecover(int nl, int nr) {
 	for(int i = 1;i <= nr;++i) if(vis[i]) ri.push_back(i);
 	return std::make_pair(le, ri);
 }
-// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-// 匈牙利，左到右单向边，bitset，O(n^2/w |match|)
-// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+// 匈牙利，左到右单向边，bitset，$O(n^2/w |match|)$
 using set = std::bitset<N>;
 set edge[N];
 bool dfs(int x, set & unvis, std::vector<int> & match) {
@@ -49,50 +47,38 @@ std::vector<int> match(int nl, int nr) {
 	for(int i = 1;i <= nr;++i) ret[match[i]] = i;
 	return ret[0] = 0, ret;
 }
-// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-// HK, 左到右单向边，O(M sqrt(|match|))
-// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-std::vector<int> edge[N];
-std::vector<int> L, R, vis, matchl, matchr;
-bool bfs(int nl, int nr) {
-	L.assign(nl + 1, 0), R.assign(nr + 1, 0);
-	static std::queue<int> Q;
-	for(int i = 1;i <= nl;++i) 
-		if(!matchl[i]) L[i] = 1, Q.push(i);
-	int succ = 0;
-	for(;Q.size();) {
-		int x = Q.front(); Q.pop();
-		for(int i : edge[x]) if(!R[i]) {
-			R[i] = L[x] + 1;
-			if(int p = matchr[i]) L[p] = R[i] + 1, Q.push(p);
-			else succ = 1;
-		}
-	}
-	return succ;
-}
-bool dfs(int x) {
-	for(int i : edge[x]) if(R[i] == L[x] + 1 && !vis[i]) {
-		vis[i] = 1;
-		if(!matchr[i] || dfs(matchr[i])) 
-			return matchl[x] = i, matchr[i] = x, 1;
-	}
-	return 0;
-}
+// HK, 左到右单向边，$O(M \sqrt{|match|})$
+std::vector<int> e[N];
+std::vector<int> matchl, matchr, a, p; 
 std::vector<int> match(int nl, int nr) {
-	matchl.assign(nl + 1, 0);
-	matchr.assign(nr + 1, 0);
-	vis.resize(nr + 1);
-	for(;bfs(nl, nr);) {
-		memset(vis.data(), 0, vis.size() << 2);
-		for(int i = 1;i <= nl;++i) if(!matchl[i]) dfs(i);
+	matchl.assign(nl + 1, 0), matchr.assign(nr + 1, 0);
+	for(;;) {
+		a.assign(nl + 1, 0), p.assign(nl + 1, 0);
+		static std::queue<int> Q;
+		for(int i = 1;i <= nl;++i) 
+			if(!matchl[i]) a[i] = p[i] = i, Q.push(i);
+		int succ = 0;
+		for(;Q.size();) {
+			int x = Q.front(); Q.pop();
+			if(matchl[a[x]]) continue;
+			for(int y : e[x]) {
+				if(!matchr[y]) {
+					for(succ = 1;y;x = p[x])
+						matchr[y] = x, std::swap(matchl[x], y);
+					break;
+				}
+				if(!p[matchr[y]])
+					Q.push(y = matchr[y]), p[y] = x, a[y] = a[x];
+			}
+		}
+		if(!succ) break;
 	}
 	return matchl;
-}
-/* 最小边覆盖，很可能没用，真别抄 */
+} // matchl 是左边每个点匹配的右边点编号
 std::pair<std::vector<int>, std::vector<int>> minedgecover(int nl, int nr) {
-	auto res = match(nl, nr);
-	std::vector<int> ls, rs;
-	for(int i = 1;i <= nl;++i) if(!L[i]) ls.push_back(i);
-	for(int i = 1;i <= nr;++i) if(R[i]) rs.push_back(i);
-	return std::make_pair(ls, rs);
+	match(nl, nr);
+	std::vector<int> l, r;
+	for(int i = 1;i <= nl;++i) if(!a[i]) l.push_back(i);
+	for(int i = 1;i <= nr;++i) if(a[matchr[i]]) r.push_back(i);
+	return {l, r};
 }
