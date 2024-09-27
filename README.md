b4efce88eee642756732cb37b8e94da71a4ad1e7
skip2004 <1228412312@qq.com>
2024-09-27 16:38:23 +0800

    qwq

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# Let-it-Rot-ICPC-Template
-pb cute
-h
-hello i'm fsy
+# ICPC-Template
+
+## 目录
+源代码在 `./contents/` 里。
+
+tex 源码在 `./main.tex/`。
+
+## Requirements:
+
+### Package minted
+```
+pip install Pygments
+```
+Add Python Scripts where Pygments installed to your system path (e.g. `AppData\Roaming\Python\Python38\Scripts` on Windows, and `pygmentize.exe` should exist)
+
+Add `-shell-escape` to your TeX arguments.
--- contents/string/manacher.cpp
+++ contents/string/manacher.cpp
@@ -1,8 +1,3 @@
-#include<bits/stdc++.h>
-using std::cin;
-using std::cout;
-using ll = long long;
-using u64 = unsigned long long;
 std::vector<int> manacher(const std::string & s, bool is_even) {
 	const int N = s.size() - is_even;
 	std::vector<int> o(N); // 半径
@@ -17,31 +12,4 @@ std::vector<int> manacher(const std::string & s, bool is_even) {
 		}
 	}
 	return o;
-}
-std::vector<int> naive(const std::string & s, bool is_even) {
-	const int N = s.size() - is_even;
-	std::vector<int> o(N);
-	for(int i = 0;i < N;++i) {
-		int & x = o[i] = 0;
-		for(;i - x >= 0 && i + x < N && s[i - x] == s[i + x + is_even];) {
-			++ x;
-		}
-	}
-	return o;
-}
-int main() {
-	std::ios::sync_with_stdio(false), cin.tie(0);
-	std::mt19937 gen;
-	for(int i = 0;i < 1e5;++i) {
-		int n = gen() % 30 + 1, is_even = gen() % 2;
-		std::string s;
-		for(int i = 0;i < n;++i) {
-			s += char('a' + std::min(gen() % 3, gen() % 3));
-		}
-		auto A = manacher(s, is_even), B = naive(s, is_even);
-		if(A != B) {
-			std::cerr << s << ' ' << is_even << '\n';
-			return 0;
-		}
-	}
 }
