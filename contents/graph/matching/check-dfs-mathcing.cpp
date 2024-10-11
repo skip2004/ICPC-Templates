9a2600c7402dd7575adccf250ca7bb4a96461016
skip2004 <1228412312@qq.com>
2024-10-11 14:10:11 +0800

    matching update

--- contents/graph/matching/check-dfs-mathcing.cpp
+++ contents/graph/matching/check-dfs-mathcing.cpp
@@ -1,3 +1,11 @@
+#include<bits/stdc++.h>
+namespace rgs = std::ranges;
+using std::cin, std::cout;
+using ll = long long;
+using u64 = unsigned long long;
+using db = double;
+const int N = 505;
+
 std::vector<int> edge[N];
 std::vector<int> match(int nl, int nr) {
 	std::vector<int> vis(nr + 1), match(nr + 1), ret(nl + 1);
@@ -28,4 +36,30 @@ std::array<std::vector<int>, 3> minedgeconver(int nl, int nr) {
 	for(int i = 1;i <= nl;++i) if(ret[i] && !vis[ret[i]]) le.push_back(i);
 	for(int i = 1;i <= nr;++i) if(vis[i]) ri.push_back(i);
 	return {le, ri, ret};
+}
+
+int main() {
+	std::ios::sync_with_stdio(false), cin.tie(0);
+	int nl, nr, m;
+	cin >> nl >> nr >> m;
+	for(int i = 1, x, y;i <= m;++i) {
+		cin >> x >> y;
+		edge[x].push_back(y);
+	}
+	auto [l, r, res] = minedgeconver(nl, nr);
+	std::vector<int> okl(nl + 1), okr(nr + 1);
+	for(int x : l) okl[x] = 1;
+	for(int x : r) okr[x] = 1;
+	for(int i = 1;i <= nl;++i) {
+		if(okl[i]) continue;
+		for(int x : edge[i]) {
+			assert(okr[x]);
+		}
+	}
+	int ans = 0;
+	for(int x : res) ans += x > 0;
+	cout << ans << '\n';
+	for(int i = 1;i <= nl;++i) {
+		cout << res[i] << " \n"[i == nl];
+	}
 }
--- contents/graph/matching/check-hk.cpp
+++ contents/graph/matching/check-hk.cpp
@@ -1,3 +1,7 @@
+#include<bits/stdc++.h>
+using std::cin;
+using std::cout;
+const int N = 100005;
 std::vector<int> e[N];
 std::vector<int> matchl, matchr, a, p; 
 std::vector<int> match(int nl, int nr) {
@@ -32,3 +36,31 @@ std::array<std::vector<int>, 3> minedgecover(int nl, int nr) {
 	for(int i = 1;i <= nr;++i) if(a[matchr[i]]) r.push_back(i);
 	return {l, r, matchl};
 }
+int main() {
+	std::ios::sync_with_stdio(false), cin.tie(0);
+	int nl, nr, m;
+	cin >> nl >> nr >> m;
+	for(int i = 0, u, v;i < m;++i) {
+		cin >> u >> v;
+		e[u + 1].push_back(v + 1);
+	}
+
+	auto [l, r, res] = minedgecover(nl, nr);
+	std::vector<int> okl(nl + 1), okr(nr + 1);
+	for(int x : l) okl[x] = 1;
+	for(int x : r) okr[x] = 1;
+	for(int i = 1;i <= nl;++i) {
+		if(okl[i]) continue;
+		for(int x : e[i]) {
+			assert(okr[x]);
+		}
+	}
+	int cc = res.size() - count(res.begin(), res.end(), 0);
+	assert(cc = l.size() + r.size());
+	cout << cc << '\n';
+	for(int i = 1;i <= nl;++i) if(res[i]) {
+		cout << i - 1 << ' ' << res[i] - 1 << '\n';
+	}
+}
+
+
